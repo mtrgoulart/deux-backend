060d96058de71d0489986bd527d5525736346efc
finn <mr.goulart@outlook.com>
2025-08-04 21:45:01 -0300

    ajustado select das strategies para trazer o symbol da instancia

--- queries/select_sell_strategy_by_instance.sql
+++ queries/select_sell_strategy_by_instance.sql
@@ -1,6 +1,6 @@
 SELECT
-    s.id,   
-    s.symbol,
+    s.id,
+    i.symbol,
     s.percent,
     s.condition_limit,
     s.interval,
@@ -11,6 +11,7 @@ FROM
     instance_strategy AS ist
 JOIN
     strategy AS s ON ist.strategy_id = s.id
+join instances i on i.id =ist.instance_id
 WHERE
     ist.instance_id = %s
     AND s.side='sell';
