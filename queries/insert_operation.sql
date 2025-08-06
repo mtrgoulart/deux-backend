784486977db9b8df876639f107e3f142d209e46a
finn <mr.goulart@outlook.com>
2025-08-05 22:07:40 -0300

    fix insert_operation adicionado returning id

--- queries/insert_operation.sql
+++ queries/insert_operation.sql
@@ -1,3 +1,3 @@
--- Em insert_operation.sql
 INSERT INTO public.operations (user_id, api_key, symbol, side, size, price, instance_id, status, "date")
-VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+RETURNING id;
