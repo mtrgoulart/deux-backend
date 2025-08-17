e0f0969931c5bee9f62cb7b12a335780c87e0bbe
finn <mr.goulart@outlook.com>
2025-08-16 23:00:19 -0300

    fix: insert_operation.sql corrigido oreder_response

--- queries/insert_operation.sql
+++ queries/insert_operation.sql
@@ -1,3 +1,3 @@
-INSERT INTO public.operations (user_id, api_key, symbol, side, size, oreder_response, instance_id, status, "date")
+INSERT INTO public.operations (user_id, api_key, symbol, side, size, order_response, instance_id, status, "date")
 VALUES (%s, %s, %s, %s, %s, %s, %s, %s, NOW())
 RETURNING id;
