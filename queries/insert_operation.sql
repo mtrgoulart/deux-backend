d05fb377ee1bf356afe2d22ffe97e9f4b99d0c90
finn <mr.goulart@outlook.com>
2025-08-16 22:58:33 -0300

    fix: insert_operation.sql adicionado now()

--- queries/insert_operation.sql
+++ queries/insert_operation.sql
@@ -1,3 +1,3 @@
 INSERT INTO public.operations (user_id, api_key, symbol, side, size, oreder_response, instance_id, status, "date")
-VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s, NOW())
 RETURNING id;
